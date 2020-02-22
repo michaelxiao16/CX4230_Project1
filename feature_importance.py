826a0ae06f1c15394095ee02c8d55fb21c002e41
J. Austin Miles <j.austinmiles@outlook.com>
2020-02-22 15:17:25 -0500

    add functionality for normalization and feature selection based on simulation requirements.

--- feature_importance.py
+++ feature_importance.py
@@ -1,25 +1,34 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import normalize
 
 
 def main():
+    """
+    Pull data from csv file, filter out all features not relevant to the simulation. Normalize all vectors so all
+    features are between 0 and 1, then perform regression. Return the sorted list of features for use in the
+    simulation
+    :return: features (unsorted), coefficients, and sorted_features
+    """
     with open("static/boston_housing.csv") as f:
         features = f.readline()
         features = features.split(',')
         # features[-1] = "medv"
         del features[-1]
-        del features[3]
-        del features[4]
+        del features[9:12]
+        del features[1:7]
     data = np.genfromtxt('static/boston_housing.csv', delimiter=',', dtype=float)
     data = data[1:]
-    data = np.hstack((data[:, 0:3], data[:, 5:]))
+    data = np.hstack((data[:, 0:1], data[:, 7:9], np.reshape(data[:, 10], (data.shape[0], 1)),
+                      np.reshape(data[:, 12], (data.shape[0], 1))))
     data_x = data[:, :-1]
     data_y = data[:, -1]
+    data_x = normalize(data_x, axis=1)
     reg = LinearRegression().fit(data_x, data_y)
     coeffs = reg.coef_
     score = reg.score(data_x, data_y)
     print(score)
-    sorted_features = [x for _,x in sorted(zip(coeffs, features))]
+    sorted_features = [x for _, x in sorted(zip(coeffs, features))]
     return features, coeffs, sorted_features
 
 
